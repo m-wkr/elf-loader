311a3012c0388f7e257e83e429f85a202336ecc7
m-wkr <182266556+m-wkr@users.noreply.github.com>
2025-11-08 19:17:02 +0000

    Added rudimentary bit manipulation

--- main.c
+++ main.c
@@ -5,26 +5,51 @@ enum error {
   FILE_ISSUE
 };
 
+enum endianness {
+  E_LOW,
+  E_HIGH
+};
+
 void error_handler(const enum error error_code) {
   if (error_code != NO_ERROR) {
     printf("File could not be opened, please ensure that it exists\n");
   }
 }
 
-void getBit(const int offset) {
+int getBit(const enum endianness endian_type,const int offset,const int size,const int *value) {
+  unsigned int binary_buffer = 0;
+
+  if (endian_type == E_LOW) {
+
+    for (int i = 0; i < 64; i++) {
+      binary_buffer *= 2;
+      binary_buffer |= (*value >> (64 - i - 1)) & 1;
+    }
+
+  } else {
+
+    for (int i = 0; i < 64; i++) {
+      binary_buffer *=2;
+      binary_buffer |= (*value >> i) & 1;
+    }
+
+  }
+
+  return binary_buffer;
 }
 
 
 enum error readBinary(const char* file_name) {
   FILE *fptr = fopen(file_name,"rb");
 
-  int buffer[1024];
+  unsigned int buffer[1024];
 
   fread(buffer,sizeof(buffer),1,fptr);
 
-  for (int i = 0; i < 1024; i++) {
-    printf("%x ",buffer[i]);
-  }
+  int testNum = 0x457f464c;
+
+  //printf("%x\n",testNum);
+  printf("%x\n",getBit(E_LOW,0,8,buffer));
 
   fclose(fptr);
 }
@@ -33,8 +58,6 @@ enum error readBinary(const char* file_name) {
 
 int main(int argc, char **argv) {
   if (argc > 1) {
-    printf("%s\n",argv[1]);
-
     readBinary(argv[1]);
   }
   return 0;
