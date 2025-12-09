180e7ac0899ce2283155b140c629c9e2386c3334
m-wkr <182266556+m-wkr@users.noreply.github.com>
2025-12-09 23:19:42 +0000

    Added elf file content structs

--- main.c
+++ main.c
@@ -1,4 +1,57 @@
 #include <stdio.h>
+#include <stdint.h>
+
+#define EI_NIDENT 16
+
+typedef uint64_t    Elf64_Addr;
+typedef uint16_t    Elf64_Half;
+typedef uint64_t    Elf64_Off;
+typedef uint32_t    Elf64_Sword;
+typedef uint32_t    Elf64_Word;
+
+
+typedef struct {
+  unsigned char e_ident[EI_NIDENT];
+  Elf64_Half      e_type;
+  Elf64_Half      e_machine;
+  Elf64_Word      e_version;
+  Elf64_Addr      e_entry;
+  Elf64_Off       e_phoff;
+  Elf64_Off       e_shoff;
+  Elf64_Word      e_flags;
+  Elf64_Half      e_ehsize;
+  Elf64_Half      e_phentsize;
+  Elf64_Half      e_phnum;
+  Elf64_Half      e_shentsize;
+  Elf64_Half      e_shnum;
+  Elf64_Half      e_shstrndx;
+} Elf64_Elf_Hdr;
+
+
+typedef struct {
+  Elf64_Word      sh_name;
+
+  Elf64_Word      sh_type;
+  Elf64_Word      sh_flags;
+  Elf64_Addr      sh_addr;
+  Elf64_Off       sh_offset;
+  Elf64_Word      sh_size;
+  Elf64_Word      sh_link;
+  Elf64_Word      sh_info;
+  Elf64_Word      sh_addralign;
+  Elf64_Word      sh_entsize;
+} Elf64_Section_Hdr;
+
+typedef struct {
+  Elf64_Word      p_type;
+  Elf64_Off       p_offset;
+  Elf64_Addr      p_vaddr;
+  Elf64_Addr      p_paddr;
+  Elf64_Word      p_filesz;
+  Elf64_Word      p_memsz;
+  Elf64_Word      p_flags;
+  Elf64_Word      p_aligns;
+} Elf64_Program_Hdr;
 
 enum error {
   NO_ERROR,
@@ -57,7 +110,7 @@ enum error readBinary(const char* file_name) {
 
   fread(buffer,1,1024,fptr);
 
-  printf("%x\n",getBit(E_LOW,0,4,buffer));
+  printf("%x\n",getBit(E_LOW,18,8,buffer));
 
   fclose(fptr);
 }
